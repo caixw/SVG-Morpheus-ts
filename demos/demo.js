9c4f9cafd560f5d064fd3255d62350b615a9612f
王云海 <adoin@qq.com>
2025-06-03 16:12:42 +0800

    test try fix svg lost

--- demos/demo.js
+++ demos/demo.js
@@ -293,7 +293,7 @@ async function init() {
       'diamond': `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <polygon points="12,2 22,12 12,22 2,12" fill="currentColor"/>
       </svg>`,
-      'vite': '/vite.svg'
+      'vite': 'vite.svg'
     };
 
     try {
--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -9,6 +9,7 @@ const version = packageJson.version;
 export default defineConfig({
   root: 'demos',
   base: './',
+  publicDir: '../public', // 指向项目根目录的public文件夹
   build: {
     outDir: '../dist-demo',
     emptyOutDir: true,
