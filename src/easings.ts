ea481a75c6d1aa910cb922eca94edd5381f339d6
caixw <xnotepad@gmail.com>
2025-08-14 12:13:12 +0800

    perf: 重构 elastic-in 和 elastic-out
    将部分变量常量化

--- src/easings.ts
+++ src/easings.ts
@@ -10,18 +10,20 @@ export const easings = {
     ['cubic-out']: (t: number) => (--t) * t * t + 1,
     ['cubic-in-out']: (t: number) => t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
     ['elastic-in']: (t: number) => {
-        var s = 1.70158; var p = 0; var a = 1;
-        if (t == 0) return 0; if (t == 1) return 1; if (!p) p = .3;
-        if (a < Math.abs(1)) { a = 1; var s = p / 4; }
-        else var s = p / (2 * Math.PI) * Math.asin(1 / a);
-        return -(a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t - s) * (2 * Math.PI) / p));
+        if (t == 0) { return 0; }
+        if (t == 1) { return 1; }
+
+        const p = .3;
+        const s = p / (2 * Math.PI) * Math.asin(1);
+        return -(Math.pow(2, 10 * (t -= 1)) * Math.sin((t - s) * (2 * Math.PI) / p));
     },
     ['elastic-out']: (t: number) => {
-        var s = 1.70158; var p = 0; var a = 1;
-        if (t == 0) return 0; if (t == 1) return 1; if (!p) p = .3;
-        if (a < Math.abs(1)) { a = 1; var s = p / 4; }
-        else var s = p / (2 * Math.PI) * Math.asin(1 / a);
-        return a * Math.pow(2, -10 * t) * Math.sin((t - s) * (2 * Math.PI) / p) + 1;
+        if (t == 0) { return 0; }
+        if (t == 1) { return 1; }
+
+        const p = .3;
+        const s = p / (2 * Math.PI) * Math.asin(1);
+        return Math.pow(2, -10 * t) * Math.sin((t - s) * (2 * Math.PI) / p) + 1;
     },
     ['elastic-in-out']: (t: number) => {
         if (t == 0) { return 0; }
